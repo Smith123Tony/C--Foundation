b74b23a427f72f1c0fa6328373d4e163299eeb8d
Smith123Tony <JK.Tony@outlook.com>
2022-11-09 00:01:58 +0800

    函数

---
+++ test11_8.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int max = get_max(a, b);
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//void test()
+//{
+//	printf("asf\n");
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
