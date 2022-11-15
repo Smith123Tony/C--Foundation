01d9ce51104153151df0e53e91cfa57e604ea3d5
Smith123Tony <JK.Tony@outlook.com>
2022-11-15 23:03:34 +0800

    未知

---
+++ test_11_15.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int seconds = 0;
+//	scanf("%d", &seconds);
+//	printf("%d %d %d\n", seconds / 3600, seconds / 60 % 60, seconds % 60);
+//	return 0;
+//}
