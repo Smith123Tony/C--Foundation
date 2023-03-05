bbc5e989b963285adbb37491a94a65a5e937ae85
Smith123Tony <JK.Tony@outlook.com>
2023-03-05 23:07:07 +0800

    practise

---
+++ test_23_3_5.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+////pta-7-10 ¼ÆËã¹¤×Ê
+//#include<stdio.h>
+//int main()
+//{
+//    int workyear = 0;
+//    int worktime = 0;
+//    scanf("%d %d", &workyear, &worktime);
+//    double salary = 0;
+//    if (workyear <= 4)
+//    {
+//        if (worktime <= 40)
+//        {
+//            salary = worktime * 30;
+//            printf("%.2lf", salary);
+//            return 0;
+//        }
+//        else
+//        {
+//            salary = 1200.0 + (worktime - 40) * 1.5 * 30;
+//            printf("%.2lf", salary);
+//            return 0;
+//        }
+//    }
+//    else
+//    {
+//        if (worktime <= 40)
+//        {
+//            salary = worktime * 50;
+//            printf("%.2lf", salary);
+//            return 0;
+//        }
+//        else
+//        {
+//            salary = 2000.0 + (worktime - 40) * 1.5 * 50;
+//            printf("%.2lf", salary);
+//            return 0;
+//        }
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
